92b48d37d2c2dc20d68315510cd39a373f0db59a
MakeMeLOLZ101 <ianchesley@yahoo.com>
2024-11-25 11:50:46 -0800

    starting to understand this

---
+++ Quiz-js-dom-asg/scripts/app.js
@@ -0,0 +1,12 @@
+document.addEventListener('readystatechange', () => {
+    const boxNumberInput = document.getElementById('boxNumber');
+    const colorInput = document.getElementById('changeColor');
+    const changeColorBtn = document.querySelector('.btn-primary:first-child');
+    const resetColorBtn = document.querySelector('.btn-primary:last-child');
+
+    //Colors Start
+    const validColors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'purple', 'brown', 'grey', 'white', 'black'];
+    //Colors End
+
+    
+});
